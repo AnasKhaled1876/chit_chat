f7b6c5e45cc1759d6ced4f4a147648621b3f7f83
AnasKhaled1876 <Anas.khaled1892@gmail.com>
2022-02-08 13:29:30 +0300

    First Commit logo Page

---
+++ lib/logo_page.dart
@@ -0,0 +1,18 @@
+import 'package:chit_chat/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class LogoPage extends StatelessWidget {
+  const LogoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2),() {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpPage()));
+    });
+    return Container(
+      color: const Color(0xFF2e98f0),
+      child:
+        Center(child: Image.asset("assets/logo.png"))
+    );
+  }
+}
