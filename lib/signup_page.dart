cc155ddf643db2d978c3d9e0361d45f487f3af9a
AnasKhaled1876 <Anas.khaled1892@gmail.com>
2022-02-08 13:32:51 +0300

    Checking Commit

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
-
+//pushed
   @override
   Widget build(BuildContext context) {
     return Container();
